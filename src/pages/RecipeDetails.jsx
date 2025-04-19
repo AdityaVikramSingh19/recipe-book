b806e24fa69034e60769c37b27f3534eab56553e
Aditya <mamtasinghahana123@gmail.com>
2025-04-19 19:25:31 +0530

    RecipeDetails updated

---
+++ src/pages/RecipeDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RecipeDetails = () => {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h2>Recipe Details Page</h2>
+      <p>This is where full recipe details will show.</p>
+    </div>
+  );
+};
+
+export default RecipeDetails;
