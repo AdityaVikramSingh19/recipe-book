2578654e2c285607005c36cdea0fbf54f9611fe9
Aditya <mamtasinghahana123@gmail.com>
2025-04-19 18:46:36 +0530

    NavBar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css'; 
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <Link to="/">🍽️ RecipeBook</Link>
+      </div>
+      <ul className="nav-links">
+        <li><Link to="/recipes">Recipes</Link></li>
+        <li><Link to="/add">Add Recipe</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
