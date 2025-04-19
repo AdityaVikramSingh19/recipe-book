2558bd4c7ff1149459c9507f76a83c161305a405
Aditya <mamtasinghahana123@gmail.com>
2025-04-19 18:42:49 +0530

    Folder Structure Organising

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Recipes from './pages/Recipes';
+import RecipeDetails from './pages/RecipeDetails';
+import AddRecipe from './pages/AddRecipe';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/recipes" element={<Recipes />} />
+        <Route path="/recipes/:id" element={<RecipeDetails />} />
+        <Route path="/add" element={<AddRecipe />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
