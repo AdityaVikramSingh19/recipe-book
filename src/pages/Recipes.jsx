8e02087a90a28cca12bf3bade69f2f60fbd957c6
Aditya <mamtasinghahana123@gmail.com>
2025-04-19 19:26:36 +0530

    Recipie.jsx updated

---
+++ src/pages/Recipes.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Recipes = () => {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h2>Recipes Page</h2>
+      <p>This is where we will list all recipes later.</p>
+    </div>
+  );
+};
+
+export default Recipes;
